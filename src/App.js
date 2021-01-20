3b1bd0cc58e0d47dabc22eda61dc131d907e7dac
Junaid S <intelpik@gmail.com>
2021-01-20 21:52:01 +0530

    finished styling first navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import {BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar/>
-    </>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Nav,NavLink,Bars,NavMenu,NavBtn,NavBtnLink} from './NavbarElements';
 
 const Navbar = () => {
     return (
