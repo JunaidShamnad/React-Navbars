eaf75a19c1b4bfcab9ef769e37d935651080dea3
Junaid S <intelpik@gmail.com>
2021-01-20 22:43:34 +0530

    finished creating second navbar

---
+++ Navbar-2/src/components/Navbar/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Nav,NavLink,Bars,NavMenu,NavBtn,NavBtnLink} from './NavbarElements';
+
+const Navbar = () => {
+    return (
+        <>
+          <Nav>
+              <NavLink to="/">
+                   <img src={require('../../images/logo.svg').default} alt="Logo"/>
+              </NavLink>
+              <Bars/>
+              <NavMenu>
+                  <NavLink to="/about" activeStyle>
+                      About
+                  </NavLink>
+                  <NavLink to="/services" activeStyle>
+                      Services
+                  </NavLink>
+                  <NavLink to="/contact-us" activeStyle>
+                      Contact Us
+                  </NavLink>
+                  <NavLink to="/sign-up" activeStyle>
+                      Sign Up
+                  </NavLink>
+                  <NavBtnLink to="/sign-in">Sign In</NavBtnLink>
+              </NavMenu>
+              {/* <NavBtn>
+                  <NavBtnLink to="/sign-in">Sign In</NavBtnLink>
+              </NavBtn> */}
+          </Nav>
+        </>
+    )
+}
+
+export default Navbar;
