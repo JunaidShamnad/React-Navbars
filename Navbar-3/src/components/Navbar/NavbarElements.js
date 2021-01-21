98ec794f5d14149999baa284a3e3612b607704a8
Junaid S <intelpik@gmail.com>
2021-01-21 07:01:02 +0530

    created sidebar elements and maked the third navbar responsive

--- Navbar-3/src/components/Navbar/NavbarElements.js
+++ Navbar-3/src/components/Navbar/NavbarElements.js
@@ -1,17 +1,55 @@
 import styled from 'styled-components';
-import {NavLink as Link} from 'react-router-dom';
+import {NavLink as LinkR} from 'react-router-dom';
+import { Link as LinkS } from "react-scroll";
 import {FaBars} from 'react-icons/fa';
 
 export const Nav=styled.nav`
     background:#000;
     height: 80px;
+    /* margin-top:-80px; */
     display:flex;
     justify-content: flex-start;
     padding: 0.5rem calc((100vw - 1000px) /2);
+    position: sticky;
+     top: 0;
     z-index:10;
+
+    @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
+
 `;
 
-export const NavLink=styled(Link)`
+export const NavbarContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  /* height: 80px; */
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1100px;
+`;
+
+export const MobileIcon = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    position: absolute;
+    top: 0;
+    right: 0;
+    transform: translate(-100%, 60%);
+    font-size: 1.8rem;
+    cursor: pointer;
+    color: #fff;
+  }
+`;
+
+export const NavItem = styled.li`
+  /* height:80px; */
+`;
+
+export const NavLink=styled(LinkS)`
     color:#fff;
     display:flex;
     align-items:center;
@@ -22,7 +60,13 @@ export const NavLink=styled(Link)`
 
     &.active{
         color: #15cdfc;
+        border-bottom: 3px solid #01bf71;
     }
+
+    &:hover {
+    color:#2477ff;
+    transition: 0.3s ease-in-out;
+  }
 `;
 
 export const Bars=styled(FaBars)`
@@ -63,11 +107,12 @@ export const NavBtn=styled.nav`
     }
 `;
 
-export const NavBtnLink=styled(Link)`
+export const NavBtnLink=styled(LinkR)`
     border-radius:4px;
     background: rgb(37,108,225);
     background: linear-gradient(117deg, rgba(37,108,225,1) 0%, rgba(22,98,224,1) 20%, rgba(8,83,204,1) 41%, rgba(2,76,195,1) 59%, rgba(12,79,189,1) 85%);
     padding:10px 22px;
+    white-space: nowrap;
     color: #fff;
     border:none;
     outline:none;
--- Navbar-3/src/components/Navbar/index.js
+++ Navbar-3/src/components/Navbar/index.js
@@ -1,34 +1,111 @@
-import React from 'react';
-import {Nav,NavLink,Bars,NavMenu,NavBtn,NavBtnLink} from './NavbarElements';
+import React, { useState, useEffect } from "react";
+import { IconContext } from "react-icons/lib";
+import { animateScroll as scroll } from "react-scroll";
+import { FaBars } from "react-icons/fa";
+import {
+  Nav,
+  NavbarContainer,
+  MobileIcon,
+  NavItem,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
+} from "./NavbarElements";
 
-const Navbar = () => {
-    return (
-        <>
-          <Nav>
-              <NavLink to="/">
-                   <img src={require('../../images/logo.svg').default} alt="Logo"/>
-              </NavLink>
-              <Bars/>
-              <NavMenu>
-                  <NavLink to="/about" activeStyle>
-                      About
-                  </NavLink>
-                  <NavLink to="/services" activeStyle>
-                      Services
-                  </NavLink>
-                  <NavLink to="/contact-us" activeStyle>
-                      Contact Us
-                  </NavLink>
-                  <NavLink to="/sign-up" activeStyle>
-                      Sign Up
-                  </NavLink>
-              </NavMenu>
-              <NavBtn>
-                  <NavBtnLink to="/sign-in">Sign In</NavBtnLink>
-              </NavBtn>
-          </Nav>
-        </>
-    )
-}
+const Navbar = ({ toggle }) => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const chnageNav = () => {
+    if (window.scrollY >= 80) {
+      setScrollNav(true);
+    } else {
+      setScrollNav(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", chnageNav);
+  }, []);
+
+  const toggleHome = () => {
+    scroll.scrollToTop();
+  };
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav scrollNav={scrollNav}>
+          <NavbarContainer>
+            <NavLink to="/" onClick={toggleHome}>
+              <img src={require("../../images/logo.svg").default} alt="Logo" />
+            </NavLink>
+            <MobileIcon onClick={toggle}>
+              <FaBars />
+            </MobileIcon>
+            <NavMenu>
+              <NavItem>
+                <NavLink
+                  to="/about"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  activeStyle
+                >
+                  About
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  to="/services"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  activeStyle
+                >
+                  Services
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  to="/contact-us"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  activeStyle
+                >
+                  Contact Us
+                </NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink
+                  to="/sign-up"
+                  smooth={true}
+                  duration={500}
+                  spy={true}
+                  exact="true"
+                  offset={-80}
+                  activeStyle
+                >
+                  Sign Up
+                </NavLink>
+              </NavItem>
+            </NavMenu>
+            <NavBtn>
+              <NavBtnLink to="/sign-in">Sign In</NavBtnLink>
+            </NavBtn>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  );
+};
 
 export default Navbar;
